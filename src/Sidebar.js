9a133f320dc3b373ab18998f96f6ef00e6f95d45
Herizoran <herizo.ran@gmail.com>
2022-06-15 21:27:14 +0300

    updated Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -78,21 +78,29 @@ class SideNavbar extends Component{
           <main>
             <MainContent/>
           </main>
-          <footer className="py-4 bg-light mt-auto">
-            <div className="container-fluid px-4">
-              <div className="d-flex align-items-center justify-contentBetween small">
-                <div className="text-muted">Copyright © Your Website 2022</div>
-                <div>
-                  <a href="#">Privacy Policy</a>
-                  ·
-                  <a href="#">Terms &amp; Conditions</a>
-                </div>
-              </div>
-            </div>
-          </footer>
+          <FooterPage/>
         </div>
       </div>
     );
+  }
+}
+
+class FooterPage extends Component{
+  render() {
+    return(
+      <footer className="py-4 bg-light mt-auto">
+        <div className="container-fluid px-4">
+          <div className="d-flex align-items-center justify-contentBetween small">
+            <div className="text-muted">Copyright © Your Website 2022</div>
+            <div>
+              <a href="#">Privacy Policy</a>
+              ·
+              <a href="#">Terms &amp; Conditions</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      );
   }
 }
 
